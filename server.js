aa8b950a5b949d26af603b7ca47324a9988f2156
nguyent0275 <nguyent0275@gmail.com>
2024-01-06 12:09:34 -0500

    added views folder and set up the handlebars templates

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+const path = require("path");
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 // import sequelize connection
+const exphbs = require("express-handlebars");
+
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(routes);
 
