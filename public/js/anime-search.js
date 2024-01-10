9bf907d13e0d2b0a997c4b93ac57cebea7d0207c
nguyent0275 <nguyent0275@gmail.com>
2024-01-09 20:23:02 -0500

    added anime search and request handlebars

--- public/js/anime-search.js
+++ public/js/anime-search.js
@@ -1,10 +1,21 @@
 const newFormHandler = async (event) => {
   event.preventDefault(event);
 
-  const title = document.querySelector("anime-name").value.trim();
-  console.log(title)
+  //   getting the user value from the search bar
+  const title = document.querySelector(".anime-name").value.trim();
+  console.log(title);
 
+  const animeDiv = document.querySelector(".anime-div");
+
+  //   clears the html after each search
+  while (animeDiv.hasChildNodes()) {
+    animeDiv.removeChild(animeDiv.firstChild);
+  }
+
+  //   if there is a value, run the function
   if (title) {
+    // calling the route in the controller, which fields the third party api call
+    // data is retrieved from third party api and sent back to the front end
     const response = await fetch(`/api/anime/search/${title}`, {
       method: "GET",
       headers: {
@@ -16,21 +27,38 @@ const newFormHandler = async (event) => {
     const animeData = await response.json();
     console.log(animeData);
 
-    const animeDiv = document.querySelector(".anime-div");
-
+    // if the response returned successfully, run the function
     if (response.ok) {
       for (let index = 0; index < animeData.data.length; index++) {
+        // create
         const title = document.createElement("h2");
         const imageCard = document.createElement("img");
         const description = document.createElement("p");
+        const addToListBtn = document.createElement("button");
 
+        // attr
         title.textContent = animeData.data[index].attributes.canonicalTitle;
         imageCard.src = animeData.data[index].attributes.posterImage.tiny;
         description.textContent = animeData.data[index].attributes.description;
+        addToListBtn.textContent = "Add to List";
 
+        // append
         animeDiv.append(title);
         animeDiv.append(imageCard);
         animeDiv.append(description);
+
+        const animeToSave = { title: title.textContent };
+        addToListBtn.addEventListener("click", async function (e) {
+          const response = await fetch(`/api/status`, {
+            method: "POST",
+            body: JSON.stringify(animeToSave),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          //   if (response.ok) {
+          //   }
+        });
       }
     }
   }
