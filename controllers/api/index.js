3cacee369b5fd01a557c946330454ce624276ada
Bogdan Tkachuk <cnndo14@gmail.com>
2024-01-10 14:11:08 -0500

    added myanimelist api for top rated, upcoming and seasonal anime

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,14 @@ const userRoutes = require('./user-routes');
 const animeRoutes = require('./anime-routes');
 const statusRoutes = require('./status-routes');
 const categoryRoutes = require('./category-routes');
-const categoryNameRoutes = require('./categoryNames-routes')
+const categoryNameRoutes = require('./categoryNames-routes');
+const animeRatingRoutes = require('./animeRating-routes');
 
 router.use('/user', userRoutes);
 router.use('/anime', animeRoutes);
 router.use('/status', statusRoutes);
 router.use('/categories', categoryRoutes);
-router.use('/categoryNames', categoryNameRoutes)
+router.use('/categoryNames', categoryNameRoutes);
+router.use('/rating', animeRatingRoutes);
 
 module.exports = router;
